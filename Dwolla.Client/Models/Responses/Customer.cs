4165be6e269b5b385b1cab9bca6ff191fe5e8a16
GravlLift <jason.gravell@gmail.com>
2020-06-05 17:53:27 -0400

    Update Customer object to use Guid instead of string

--- Dwolla.Client/Models/Responses/Customer.cs
+++ Dwolla.Client/Models/Responses/Customer.cs
@@ -4,7 +4,7 @@ namespace Dwolla.Client.Models.Responses
 {
     public class Customer : BaseResponse
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
