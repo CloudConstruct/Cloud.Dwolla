212974896f50ba0e8bec68ff54b1bc3f95e8b381
GravlLift <jason.gravell@gmail.com>
2020-06-23 16:04:49 -0400

    Remove Polly

--- Dwolla.Client/DwollaDependencyInjection.cs
+++ Dwolla.Client/DwollaDependencyInjection.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using Dwolla.Client;
-using Polly;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
