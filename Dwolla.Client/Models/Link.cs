729bd600ca6370d9f6c711eb70febcb0c595e201
GravlLift <jason.gravell@gmail.com>
2020-07-23 13:09:20 -0400

    Removed broken LinkDictionary accessors

--- Dwolla.Client/Models/Link.cs
+++ Dwolla.Client/Models/Link.cs
@@ -11,14 +11,8 @@ namespace Dwolla.Client.Models
 
         [JsonProperty(PropertyName = "resource-type")]
         public string ResourceType { get; set; }
-        public Guid? Id => Href == null || Href.Segments.Length == 0 ? (Guid?) null : Guid.Parse(Href.Segments[Href.Segments.Length - 1]);
+        public Guid? Id => Href == null || Href.Segments.Length == 0 ? (Guid?)null : Guid.Parse(Href.Segments[Href.Segments.Length - 1]);
     }
 
-    public class LinkDictionary : Dictionary<string, Link>
-    {
-        public Link Source => this["source"];
-        public Link FundingSources => this["funding-sources"];
-        public Link FundedTransfer => this["funded-transfer"];
-        public Link FundingTransfer => this["funding-transfer"];
-    }
+    public class LinkDictionary : Dictionary<string, Link> { }
 }
